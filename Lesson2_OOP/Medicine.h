cff7b283e1d3c0cf25d2e7487c5daca85c8c0c2c
dudolad_yaroslav <dudolad2009@gmail.com>
2025-07-16 23:17:56 +0300

    create pharm

---
+++ Lesson2_OOP/Medicine.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Medicine
+{
+private:
+	char* m_title;
+	char* m_type;
+	float m_price;
+public:
+	Medicine();
+	Medicine(const char* title, const char* type, float price);
+	~Medicine();
+
+	void setTitle(const char* title);
+	void setType(const char* type);
+	void setPrice(const char* price);
+
+	const char* getTitle()const;
+	const char* getType()const;
+	float getPrice()const;
+
+	void showInfo()const;
+};
+
