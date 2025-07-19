1f9ee4059c1f163a36570543da537f247d6ed68d
dudolad_yaroslav <dudolad2009@gmail.com>
2025-07-19 21:26:30 +0300

    pharm.cpp

--- Lesson2_OOP/Pharm.cpp
+++ Lesson2_OOP/Pharm.cpp
@@ -1,1 +1,42 @@
 #include "Pharm.h"
+
+Pharm::Pharm()
+{
+	m_name = new char[10] {"Undefined"};
+
+	m_size_medicines = 0;
+	m_arr = nullptr;
+
+}
+
+Pharm::Pharm(const char* name)
+{
+	int size_name = strlen(name) + 1;
+	m_name = new char[size_name];
+	strcpy_s(m_name, size_name, name);
+
+	m_size_medicines = 3;
+	m_arr = new Medicine[3];
+
+	m_arr[0] = Medicine("Aspiryn", "pills", 20.50);
+	m_arr[0] = Medicine("Nurofen", "syrope", 200.55);
+	m_arr[0] = Medicine("Doctor Mom", "syrope", 150.90);
+}
+
+Pharm::~Pharm()
+{
+	delete[] m_name;
+	if (m_size_medicines > 0) delete[] m_arr;
+}
+
+void Pharm::showInfo() const
+{
+	cout << "\tPharm: " << m_name << endl;
+	cout << "\tAmount of med: " << m_size_medicines << endl;
+	for (size_t i = 0; i < m_size_medicines; i++)
+	{
+		m_arr[i].showInfo();
+		cout << endl;
+	}
+
+}
